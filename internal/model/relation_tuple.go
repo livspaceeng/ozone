28ec56278dbd9ca9856516d08fbd493f5ec2da46
Yash Mehta <livspace@Admins-MacBook-Pro.local>
2022-08-24 12:26:38 +0530

    removed unnecessary models

--- internal/model/relation_tuple.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type RelationTuple struct {
-	Namespace   string `json:"namespace" example:"canvas"`
-	Object      string `json:"object" example:"project-123"`
-	Relation    string `json:"relation" example:"read"`
-	Subject_Id  string `json:"subject_id,omitempty" example:"user-123"`
-	Subject_Set SubjectSet `json:"subject_set,omitempty"`
-}
--- internal/model/subject_set.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type SubjectSet struct {
-	Namespace   string `json:"namespace" example:"canvas"`
-	Object      string `json:"object" example:"project-123"`
-	Relation    string `json:"relation" example:"read"`
-}
