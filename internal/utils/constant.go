124a103c5a69e36de76cf8b3fbdbf7158d9066fc
Yash Mehta <yashmehta@livspace.com>
2024-04-22 04:15:42 +0530

    subject check

--- internal/utils/constant.go
+++ internal/utils/constant.go
@@ -1,11 +1,13 @@
 package utils
 
 var (
-	NamespaceString = "namespace="
-	RelationString = "relation="
-	ObjectString = "object="
-	InvalidError = "Invalid query params"
-	HttpResponse = " Http Response: "
-	RelationLog = " Relation: "
-	ObjectLog = " Object: "
+	NamespaceString       = "namespace="
+	RelationString        = "relation="
+	ObjectString          = "object="
+	SubjectString         = "subject="
+	SubjectRelationString = "subject-relation="
+	InvalidError          = "Invalid query params"
+	HttpResponse          = " Http Response: "
+	RelationLog           = " Relation: "
+	ObjectLog             = " Object: "
 )
